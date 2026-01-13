f2bb32cc0820a041ab8234f38a2609ef669d6f83
Kartik Nesari <kartiknesari@gmail.com>
2026-01-13 02:44:35 -0500

    Intial Commit, porting from ts to js with complete typescript support

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import { HomePage } from "./App";
+
+// Clear the existing HTML content
+document.body.innerHTML = '<div id="app"></div>';
+
+// Render your React component instead
+const root = createRoot(document.getElementById("app"));
+
+root.render(
+    <BrowserRouter>
+        <HomePage />
+    </BrowserRouter>
+);
